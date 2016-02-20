8bb7b4057d80d2b4431c5eac102f2299009ff2cf
choies1 <choies1@gmail.com>
2016-02-20 12:53:53 +0900

    Searching_WiFi.lua
    Searching WiFi near the board

---
+++ 02_Searching_WiFi/Searching_WiFi.lua
@@ -0,0 +1,20 @@
+
+wifi.setmode(wifi.STATION)
+
+-- print ap list
+function listap(t)
+      for ssid,v in pairs(t) do
+        authmode, rssi, bssid, channel = 
+          string.match(v, "(%d),(-?%d+),(%x%x:%x%x:%x%x:%x%x:%x%x:%x%x),(%d+)")
+        print(ssid,authmode,rssi,bssid,channel)
+      end
+end
+      
+wifi.sta.getap(listap)
+tmr.delay(1000000)
+
+wifi.sta.config("Junho","chlwnsgh0910")
+wifi.sta.connect()
+tmr.delay(1000000)   -- wait 1,000,000 us = 1 second
+print(wifi.sta.status())
+print(wifi.sta.getip())
