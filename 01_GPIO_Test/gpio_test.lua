613eec40955ca929dd5bde3a80f797b99894d75d
choies1 <choies1@gmail.com>
2016-02-20 12:46:08 +0900

    GPIO_Test (LED Blinking)

---
+++ 01_GPIO_Test/gpio_test.lua
@@ -0,0 +1,14 @@
+--[[
+Test Board: WeMos D1 board (ESP8266, ESP-12F)
+--]]
+
+gpio_pin = 4
+gpio.mode(gpio_pin, gpio.OUTPUT)
+
+while 1 do
+  gpio.write(gpio_pin, gpio.HIGH)
+  tmr.delay(1000000) -- wait 1,000,000 us = 1 second
+  gpio.write(gpio_pin, gpio.LOW)
+  tmr.delay(1000000) -- wait 1,000,000 us = 1 second
+end
+
